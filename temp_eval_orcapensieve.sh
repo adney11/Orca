afbae813bace04bad849d4043144a13f56ff6496
adney11 <adney11@gmail.com>
2022-09-08 10:20:50 -0500

    eval_script for orcapensieve

--- temp_eval_orcapensieve.sh
+++ temp_eval_orcapensieve.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-# 902
-toeval=(906 907 908 909 911 913 914)
-schemes=("BB" "RB" "BOLA" "fastMPC" "robustMPC" "RL")
-for e in ${toeval[@]}; do
-    for s in ${schemes[@]}; do
-    if [$e == 906]; then
-        if [$s == "BB"] || [$s == "BOLA"] || [$s == "fastMPC"] || [$s == "RB"]; then
-            echo "skipping $s $e"
-            continue
-        fi
-    fi
+
+
+s=$1
+for e in `seq 900 999`; 
+do    
     echo "./03_orca-pensieve.sh 4 44444 $s $e"
     ./03_orca-pensieve.sh 4 44444 $s $e
     sleep 10
