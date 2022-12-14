02b51ba9e821d2facc7e6865641c3ad1c1ed34e7
acardoza <acardoza@node0.orcakernel.wisr-pg0.wisc.cloudlab.us>
2022-12-14 13:32:12 -0600

    made custom_orca_pensieve

---
+++ custom_orca_pensieve/setup_directory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# log directories
+mkdir log
+mkdir logs
+mkdir results
+
+
+# pensieve directories
+mkdir pensieve/abr_browser_dir
+mkdir pensieve/seperate_models
+mkdir pensieve/video_server
