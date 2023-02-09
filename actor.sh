30fbeab0f0d7af5226b344347774c1fb2d3bfb43
adney11 <adney11@gmail.com>
2023-02-09 17:43:23 -0500

    actor.sh

--- actor.sh
+++ actor.sh
@@ -24,9 +24,9 @@ orca_binary=${13}
 
 abr_algo=${14}
 
-echo "[$0]: Running orca-$scheme: $down"
-echo "[$0]: orca_binary is: $orca_binary"
-echo "[$0]: full path is: $path/$orca_binary"
+echo "[$0-$id]: Running orca-$scheme: $down"
+echo "[$0-$id]: orca_binary is: $orca_binary"
+echo "[$0-$id]: full path is: $path/$orca_binary"
 #exit
 
 trace=""
@@ -34,29 +34,29 @@ scheme_des="orca-$scheme-$latency-$period-$qsize"
 log="orca-$scheme-$down-$up-$latency-${period}-$qsize-$abr_algo"
 
 #Bring up the actor i:
-echo "[$0]: will be done in $finish_time seconds ..."
-echo "[$0]: $path/$orca_binary $port $path ${period} ${first_time} $scheme $id $down $up $latency $log $finish_time $qsize $max_it $abr_algo"
+echo "[$0-$id]: will be done in $finish_time seconds ..."
+echo "[$0-$id]: $path/$orca_binary $port $path ${period} ${first_time} $scheme $id $down $up $latency $log $finish_time $qsize $max_it $abr_algo"
 
-echo "[$0]: sleeping in hopes that learner is ready"
+echo "[$0-$id]: sleeping in hopes that learner is ready"
 
-echo "[$0]: slept, now calling actor stuff"
-echo "[$0]: $path/$orca_binary $port $path ${period} ${first_time} $scheme $id $down $up $latency $log $finish_time $qsize $max_it $abr_algo"
+echo "[$0-$id]: slept, now calling actor stuff"
+echo "[$0-$id]: $path/$orca_binary $port $path ${period} ${first_time} $scheme $id $down $up $latency $log $finish_time $qsize $max_it $abr_algo"
 $path/$orca_binary $port $path ${period} ${first_time} $scheme $id $down $up $latency $log $finish_time $qsize $max_it $abr_algo
-echo "[$0]: finished running $orca_binary" 
+echo "[$0-$id]: finished running $orca_binary" 
 sudo killall -s15 python $orca_binary Xvfb chrome chromedriver
 sleep 10
-echo "[$0]: Finished."
+echo "[$0-$id]: Finished."
 if [ ${first_time} -eq 2 ] || [ ${first_time} -eq 4 ]
 then
-    echo "[$0]: Doing Some Analysis ..."
-    echo "[$0]: path: $path"
+    echo "[$0-$id]: Doing Some Analysis ..."
+    echo "[$0-$id]: path: $path"
     out="sum-${log}.tr"
-    echo "[$0]: out: $out"
+    echo "[$0-$id]: out: $out"
     sudo echo $log >> $path/log/$out
     sudo perl $path/mm-thr 500 $path/log/down-${log} 1>$path/plots/plot-${log}.svg 2>res_tmp
     sudo cat res_tmp >>$path/log/$out
     sudo echo "------------------------------" >> $path/log/$out
     rm *tmp
 fi
-echo "[$0]: Done"
+echo "[$0-$id]: Done"
 
